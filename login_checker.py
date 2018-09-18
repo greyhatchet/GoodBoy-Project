686b47960f5e8848cde72b2972d9d323330ecd7f
aylwg <43182190+aylwg@users.noreply.github.com>
2018-09-18 11:18:17 -0500

    login update

--- login_checker.py
+++ login_checker.py
@@ -1,1 +1,9 @@
 #login checker
+from flask import request, redirect
+@app.route('/login', methods = ['POST'])
+def login():
+    username = request.form['username'] 
+    password = request.form['password']
+    #email = request.form['email']
+    #print("The email address is '" + email + "'")
+    return redirect('/')

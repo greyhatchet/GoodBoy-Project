1f4ca6896b0c9f9b90f40dbff35b8f2a12163487
greyhatchet <40676385+greyhatchet@users.noreply.github.com>
2018-09-20 11:49:04 -0500

    Delete login.py

--- login.py
+++
@@ -1,8 +0,0 @@
-from flask import request, redirect
-@app.route('/login', methods = ['POST'])
-def login():
-    username = request.form['username'] 
-    password = request.form['password']
-    #email = request.form['email']
-    #print("The email address is '" + email + "'")
-    return redirect('/')

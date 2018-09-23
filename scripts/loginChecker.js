387dd9ed4f651d48c368c272a14a0977c8342cdf
Tyson-Smiter <34221646+Tyson-Smiter@users.noreply.github.com>
2018-09-23 16:09:00 -0500

    loginCheck Functionality!
    First update! We lit

--- scripts/loginChecker.js
+++ scripts/loginChecker.js
@@ -1,2 +1,15 @@
-console.log(user_array[1].username + 'hi');
+// Set two variables, username and password
 
+
+function loginChecker() {
+	var login_user = document.getElementById('username').value;
+	var login_password = document.getElementById('password').value;
+
+	for (z in user_array){
+		if (user_array[z].username == login_user){
+			if(user_array[z].password == login_password){
+				return true
+			}
+		}
+	}
+}

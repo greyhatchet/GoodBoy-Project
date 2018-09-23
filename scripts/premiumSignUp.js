c4eadb764242e32b435c09cb4e920d166530ed1f
Tyson-Smiter <34221646+Tyson-Smiter@users.noreply.github.com>
2018-09-23 18:36:15 -0500

    Updates to Homepage
    Aesthetics of Homepage updated.
    Functionality Impovements as Well

--- scripts/premiumSignUp.js
+++ scripts/premiumSignUp.js
@@ -1,9 +1,9 @@
 // function to sign up new users and push object onto user array
 
-function signUp() {
+function premiumSignUp() {
 	var new_user = document.getElementById('username').value;
 	var new_password = document.getElementById('password').value;
-	var user = new user_pass_data (new_user,new_password,1);
+	var user = new user_pass_data (new_user,new_password,2);
 	user_array.push(user);
-	window.location.assign("website_outline.html");
+
 }

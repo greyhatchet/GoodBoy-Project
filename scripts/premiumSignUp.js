781816db84043cdf6cf2bb6b8b1451864e1485ab
aylwg <43182190+aylwg@users.noreply.github.com>
2018-09-23 21:26:33 -0500

    COMMIT

--- scripts/premiumSignUp.js
+++ scripts/premiumSignUp.js
@@ -5,11 +5,5 @@ function premiumSignUp() {
 	var new_password = document.getElementById('password').value;
 	var user = new user_pass_data (new_user,new_password,2);
 	user_array.push(user);
-<<<<<<< HEAD
 	redirectTo('./premium_website.html')
-=======
-	alert();
-	window.top.location = "./premiumWebsite.html";
-
->>>>>>> 5fa9039a04efdf6e91d56d4b8db42debc3dfda99
 }
--- scripts/signUp.js
+++ scripts/signUp.js
@@ -5,11 +5,6 @@ function signUp() {
 	var new_password = document.getElementById('password').value;
 	var user = new user_pass_data (new_user,new_password,1);
 	user_array.push(user);
-<<<<<<< HEAD
 	window.location.assign("website_outline.html");
 	redirectTo('./website.html')
-=======
-	alert();
-	window.top.location"./website.html";
->>>>>>> 5fa9039a04efdf6e91d56d4b8db42debc3dfda99
 }

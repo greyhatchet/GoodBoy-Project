2f36119a74f7de8ff3389846678c91407c071759
aylwg <43182190+aylwg@users.noreply.github.com>
2018-09-23 21:35:28 -0500

    jesus

--- scripts/loginChecker.js
+++ scripts/loginChecker.js
@@ -7,17 +7,10 @@ function loginChecker() {
 
 	for (z in user_array){
 		if (user_array[z].username == login_user){
-<<<<<<< HEAD
 			if(user_array[z].password == login_password){
 				//alert('Great job hon')
 				//window.location.pathname='C:/Users/Audrey/Documents/GitHub/7P1A/website.html'
 				redirectTo('./website.html')
-=======
-			if(user_array[z].password == login_password) {
-				window.top.location = "./website.html";
-			}
-
->>>>>>> 5fa9039a04efdf6e91d56d4b8db42debc3dfda99
 			}
 		}
 	}
